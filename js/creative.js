b9935fd82cbd253b6aff00e00b8af9903454208e
Roberto Arroyo <vorkelball@gmail.com>
2018-12-28 03:15:41 -0600

    avance DEMO

--- js/creative.js
+++ js/creative.js
@@ -75,7 +75,7 @@
 
   // Magnific popup calls
   $('.popup-gallery').magnificPopup({
-    delegate: 'a',
+    /*delegate: 'a',
     type: 'image',
     tLoading: 'Loading image #%curr%...',
     mainClass: 'mfp-img-mobile',
@@ -86,7 +86,7 @@
     },
     image: {
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
+    }*/
   });
 
 })(jQuery); // End of use strict
