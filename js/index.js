10ab9ef07257580db371ccbd0911293c8a4e033b
Roberto Arroyo <vorkelball@gmail.com>
2019-01-25 01:10:05 -0600

    agrego imagenes y tablero de analisis de laboratorio

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('[id^=detail-]').show();
+    /*$('.toggle').click(function() {
+        $input = $( this );
+        $target = $('#'+$input.attr('data-toggle'));
+        $target.slideToggle();
+    });*/
+});
